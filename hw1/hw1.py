827af8afaa2a5f410b7cf06b9d7e566aa5123a0f
Isaac Cohen <Isaac@3derm.com>
2014-09-14 23:01:32 -0400

    hw 1.2

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,16 +1,15 @@
 import re
 
 def tokenize(terminals, str):
-    string='('
+    string='(\s+|'
     for x in terminals:
+        if x=="+" or x=="(" or x==")" or x=="*":
+            string += '\\'
         string += x+'|'
     string = string[:(len(string)-1)]
     string+=')'
     tokens = [t for t in re.split(string, str)]
-    for x in tokens:
-        if (x=='' or x== ' '):
-            tokens.remove(x)
-    return (tokens)
+    return [t for t in tokens if not t.isspace() and not t == ""]
 
 def directions(str):
     if str[0]== 'stop' and str[1]==';':
@@ -36,3 +35,128 @@ def variable(tokens):
     if re.match(r"([a-z][A-Z]*)", tokens[0]):
         return ({"Variable": [tokens[0]]}, tokens[1:])
 
+def term(tokens):
+    if tokens[0]== '#':
+        (e1,tokens)=number(tokens[1:])
+        return (e1,tokens)
+    if tokens[0]=='@':
+        (e1,tokens)=variable(tokens[1:])
+        return (e1,tokens)
+    if tokens[0] == 'plus' and tokens[1]== '(':
+        (e1,tokens)= term(tokens[2:])
+        if tokens[0] == ',':
+            (e2,tokens)= term(tokens[1:])
+            if tokens[0] == ')':
+                return ({'Plus':[e1,e2]}, tokens[1:])
+    if tokens[0] == 'mult' and tokens[1]== '(':
+        (e1,tokens)= term(tokens[2:])
+        if tokens[0] == ',':
+            (e2,tokens)= term(tokens[1:])
+            if tokens[0] == ')':
+                return ({'Mult':[e1,e2]}, tokens[1:])
+    if tokens[0] == 'log' and tokens[1]== '(':
+        (e1,tokens) = term(tokens[2:])
+        if tokens[0]==')':
+            return ({'Log':[e1]},tokens[1:])
+def formula(tokens):
+    if tokens[0]=='true':
+       return ('True', tokens[1:])
+    if tokens[0]=='false':
+        return ('False', tokens[1:])
+    if tokens[0] == 'not' and tokens[1]== '(':
+        (e1,tokens) = formula(tokens[2:])
+        if tokens[0]==')':
+            return ({'Not':[e1]},tokens[1:])
+    if tokens[0] == 'and' and tokens[1]== '(':
+        (e1,tokens)= formula(tokens[2:])
+        if tokens[0] == ',':
+            (e2,tokens)= formula(tokens[1:])
+            if tokens[0] == ')':
+                return ({'And':[e1,e2]}, tokens[1:])
+    if tokens[0] == 'and' and tokens[1]== '(':
+        (e1,tokens)= formula(tokens[2:])
+        if tokens[0] == ',':
+            (e2,tokens)= formula(tokens[1:])
+            if tokens[0] == ')':
+                return ({'And':[e1,e2]}, tokens[1:])
+    if tokens[0] == 'or' and tokens[1]== '(':
+        (e1,tokens)= formula(tokens[2:])
+        if tokens[0] == ',':
+            (e2,tokens)= formula(tokens[1:])
+            if tokens[0] == ')':
+                return ({'Or':[e1,e2]}, tokens[1:])
+    if tokens[0] == 'or' and tokens[1]== '(':
+        (e1,tokens)= formula(tokens[2:])
+        if tokens[0] == ',':
+            (e2,tokens)= formula(tokens[1:])
+            if tokens[0] == ')':
+                return ({'Or':[e1,e2]}, tokens[1:])
+    if tokens[0] == 'equal' and tokens[1]== '(':
+        (e1,tokens)= term(tokens[2:])
+        if tokens[0] == ',':
+            (e2,tokens)= term(tokens[1:])
+            if tokens[0] == ')':
+                return ({'Equal':[e1,e2]}, tokens[1:])
+    if tokens[0] == 'greater' and tokens[1]== 'than' and tokens[2]==('('):
+        (e1,tokens)= term(tokens[3:])
+        if tokens[0] == ',':
+            (e2,tokens)= term(tokens[1:])
+            if tokens[0] == ')':
+                return ({'GreaterThan':[e1,e2]}, tokens[1:])
+    if tokens[0] == 'less' and tokens[1]== 'than' and tokens[2]==('('):
+        (e1,tokens)= term(tokens[3:])
+        if tokens[0] == ',':
+            (e2,tokens)= term(tokens[1:])
+            if tokens[0] == ')':
+                return ({'LessThan':[e1,e2]}, tokens[1:])
+
+def program(tokens):
+    tmp= tokens[0:]
+    if tmp[0]== 'print':
+       r = formula(tmp[1:])
+       if not r is None:
+            (e1, tmp)=r
+            if tmp[0]==';':
+                (e2, tmp)= program(tmp[1:])
+                return ({'Print':[e1,e2]}, tmp)
+    tmp= tokens[0:]
+    if tmp[0]== 'print':
+       r = term(tmp[1:])
+       if not r is None:
+            (e1, tmp)=r
+            if tmp[0]==';':
+                (e2, tmp)= program(tmp[1:])
+                return ({'Print':[e1,e2]}, tmp)
+    tmp = tokens[0:]
+    if tmp[0] == 'assign' and tmp[1]== '@':
+        r= variable(tmp[2:])
+        if not r is None:
+            (e1, tmp) = r
+            if tmp[0] == ':=':
+                r=term(tmp[1:])
+                if not r is None:
+                    (e2,tmp)=r
+                    if tmp[0] == ';':
+                        r= program((tmp[1:]))
+                        if not r is None:
+                            (e3, tmp )=r
+                            return ({'Assign': [e1,e2,e3]}, tmp)
+    tmp = tokens[0:]
+    if tmp[0]== 'end' and tmp[1]==';':
+        return ('End',tmp[2:])
+
+def complete(tokens):
+    #tokens =tokenize(['print', 'assign', '@', 'end', 'true', 'false', 'not', 'and', 'or', 'equal', 'less', 'than', 'greater', ',', 'plus', 'mult', 'log', '#',':=',';' ,'([1-9][0-9]*)', '([a-z][A-Z]*)','(',')' ], tokens)
+    tokens = tokenize(['end', ';','print','true', 'assign', '@', ':=', '#', 'not','plus','log','(',')','equal',',','less','than','greater', 'mult','false', 'and', 'or'], tokens)
+    r = program(tokens)
+    if r is None:
+        r= term(tokens)
+        if r is None:
+            r=formula(tokens)
+            if r is None:
+                if tokens[0]=='@':
+                    r=variable(tokens)
+                if tokens[0]=='#':
+                  r= number(tokens)
+    if r is not None:
+        return r
--- hw1/hw1test.py
+++ hw1/hw1test.py
@@ -1,5 +1,6 @@
 import hw1
 import json
+import re
 
 hw1.tokenize(["red", "blue", "#"], "red#red blue# red#blue")
 (tree,tokens)= hw1.directions(hw1.tokenize(
@@ -9,3 +10,12 @@ hw1.tokenize(["red", "blue", "#"], "red#red blue# red#blue")
 print(tree, tokens)
 
 print(hw1.variable(['123', 'foo', 'bar']))
+
+print ([t for t in re.split(r"(\s+|token)", "tokentoken")])
+
+print (hw1.formula(['less','than','(','log', '(', 'mult', '(', '#', '2', ',', '#', '3', ')', ')', ',','#','3',')']))
+
+print(hw1.complete("end ;"))
+
+print(hw1.complete("assign @y := #100; assign @x := plus(mult(log(@y),#20),#30); print and(less than(@x, @y), or(equal(@y, #200), false)); end;"))
+print(hw1.complete("assign @a := #1; assign @b := log(plus(mult(log(@a),#2),#3)); print not(greater than(log(plus(mult(log(@a),#2),#3)),log(plus(mult(log(@a),#2),#3)))); print and(or(true,false), not(true)); end;"))
